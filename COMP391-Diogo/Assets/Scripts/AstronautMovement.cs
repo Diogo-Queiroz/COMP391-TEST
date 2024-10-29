f3d98d8e03e20dc7605a57c904fe56f7ab992a24
Diogo Queiroz <diogoqueirooz1212@gmail.com>
2024-10-29 19:13:26 -0400

    Cinemachine and Confiner2D, Sounds SFX, PhysicsMaterial2D for player movement

--- COMP391-Diogo/Assets/Scripts/AstronautMovement.cs
+++ COMP391-Diogo/Assets/Scripts/AstronautMovement.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class AstronautMovement : MonoBehaviour
 {
-	private float horizontaInput;
-	[Header("Movevement Section")]
-	[SerializeField] private float speed;
-	[SerializeField] private float jumpForce;
+    private float horizontaInput;
+    [Header("Movevement Section")]
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
 
-	[Header("Flip Player on X")]
-	[SerializeField] private bool isFacingRight = true;
+    [Header("Flip Player on X")]
+    [SerializeField] private bool isFacingRight = true;
 
-	[Header("Ground Detection")]
-	[SerializeField] private Transform groundCheck;
-	[SerializeField] private LayerMask groundMask;
+    [Header("Ground Detection")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundMask;
 
-	private Rigidbody2D rb;
+    private Rigidbody2D rb;
 
     void Start()
     {
@@ -29,46 +29,46 @@ public class AstronautMovement : MonoBehaviour
     {
         horizontaInput = Input.GetAxis("Horizontal");
 
-		// Handle the Jump
-		if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
-		{
-			// Then I want to jump
-			rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-		}
+        // Handle the Jump
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            // Then I want to jump
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
 
-		if (Input.GetKeyUp(KeyCode.Space) && rb.velocity.y > 0f)
-		{
-			rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-		}
+        if (Input.GetKeyUp(KeyCode.Space) && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
 
-		Flip();
+        Flip();
     }
 
-	void FixedUpdate()
-	{
-		rb.velocity = new Vector2(horizontaInput * speed, rb.velocity.y);
-	}
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontaInput * speed, rb.velocity.y);
+    }
 
-	bool IsGrounded()
-	{
-		return Physics2D.OverlapCircle(groundCheck.position, 0.5f, groundMask);
-	}
+    bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.5f, groundMask);
+    }
 
-	void Flip()
-	{
-		Vector3 newLocalScale = transform.localScale;
+    void Flip()
+    {
+        Vector3 newLocalScale = transform.localScale;
 
-		if (horizontaInput > 0f)
-		{
-			isFacingRight = true;
-			newLocalScale.x = -1f; // It's -1 because the asset is facing left already
-		}
-		if (horizontaInput < 0f)
-		{
-			isFacingRight = false;
-			newLocalScale.x = 1f;
-		}
-		
-		transform.localScale = newLocalScale;
-	}
+        if (horizontaInput > 0f)
+        {
+            isFacingRight = true;
+            newLocalScale.x = -1f; // It's -1 because the asset is facing left already
+        }
+        if (horizontaInput < 0f)
+        {
+            isFacingRight = false;
+            newLocalScale.x = 1f;
+        }
+
+        transform.localScale = newLocalScale;
+    }
 }
